44570e8d6d73963afb0427acee82670e4804bc7a
acr0matic <work.acr0matic@gmail.com>
2023-02-14 14:34:14 +0300

    feat: Оптимизация изображений и скрипт прокрутки

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -2,3 +2,10 @@ const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
 const isMobile = window.matchMedia('(max-width: 576px)').matches;
 const isTablet = window.matchMedia('(max-width: 991px)').matches;
+
+const scrollParams = {
+  speed: 500,
+  speedAsDuration: true,
+  offset: 0,
+  updateURL: false,
+}
--- src/scripts/init.js
+++ src/scripts/init.js
@@ -8,3 +8,5 @@ const lazyLoadInstance = new LazyLoad({
     if (loader) loader.classList.add('preloader--hide');
   },
 });
+
+const scrollController = new SmoothScroll('a[href*="#"]', scrollParams);
