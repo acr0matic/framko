9a0d627dec586215a9632c817367af794b1d9531
acr0matic <work.acr0matic@gmail.com>
2023-02-14 11:26:12 +0300

    feat: верстка "тарифы" и "видео"

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -23,3 +23,28 @@ const sliderOffer = new Swiper('#slider-offer .swiper', {
   //   crossFade: true
   // },
 });
+
+const sliderPrice = new Swiper('#slider-price .swiper', {
+  slidesPerView: 2.75,
+  spaceBetween: 24,
+  grabCursor: true,
+  centeredSlides: true,
+
+  scrollbar: {
+    el: '#slider-price .swiper-scrollbar',
+    draggable: true,
+    snapOnRelease: true,
+  },
+
+  on: {
+    afterInit(instance) {
+      instance.slideTo(2);
+    },
+  },
+
+  breakpoints: {
+    1400: {
+      slidesPerView: 3
+    },
+  }
+});
